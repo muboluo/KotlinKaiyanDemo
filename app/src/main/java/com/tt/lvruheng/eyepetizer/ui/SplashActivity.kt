c9fa2daf606eedb51edbc4881be8d7ba690ef9d2
henry <muboluo@126.com>
2018-09-14 20:42:21 +0800

    第一次提交，基本完成 欢迎界面

---
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/ui/SplashActivity.kt
@@ -0,0 +1,62 @@
+package com.tt.lvruheng.eyepetizer.ui
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.ScaleAnimation
+import com.tt.lvruheng.eyepetizer.R
+import com.tt.lvruheng.eyepetizer.utils.newIntent
+import kotlinx.android.synthetic.main.activity_splash.*
+
+/**
+ * Created by lvruheng on 2017/7/2.
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //设置全屏
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+        initView()
+        setAnimation()
+
+
+    }
+
+    private fun initView() {
+        val font: Typeface = Typeface.createFromAsset(this.assets, "fonts/Lobster-1.4.otf")
+        tv_name_english.typeface = font
+        tv_english_intro.typeface = font
+    }
+
+
+    private fun setAnimation() {
+        val alphaAnimation = AlphaAnimation(0.1f, 1.0f)
+        alphaAnimation.duration = 1000
+        val scaleAnimation = ScaleAnimation(0.1f, 1.0f, 0.1f, 1.0f, ScaleAnimation.RELATIVE_TO_SELF, 0.5f, ScaleAnimation.RELATIVE_TO_SELF, 0.5f)
+        scaleAnimation.duration = 1000
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(alphaAnimation)
+        animationSet.addAnimation(scaleAnimation)
+        animationSet.duration = 1000
+        iv_icon_splash.startAnimation(animationSet)
+        animationSet.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                newIntent<MainActivity>()
+                finish()
+            }
+        })
+    }
+}
