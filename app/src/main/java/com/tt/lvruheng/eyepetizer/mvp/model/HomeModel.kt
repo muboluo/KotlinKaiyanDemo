bd3879a732637f497c570c77bbff5c13b3dda9d0
henry <muboluo@126.com>
2018-09-19 19:12:24 +0800

    完成 首页 bean 页面

--- app/src/main/java/com/tt/lvruheng/eyepetizer/mvp/model/HomeModel.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/mvp/model/HomeModel.kt
@@ -5,19 +5,19 @@ import com.tt.lvruheng.eyepetizer.mvp.model.bean.HomeBean
 import com.tt.lvruheng.eyepetizer.network.ApiService
 import com.tt.lvruheng.eyepetizer.network.RetrofitClient
 import io.reactivex.Observable
-import io.reactivex.functions.Function
 
 /**
  * Created by lvruheng on 2017/7/5.
  */
-class HomeModel{
-    fun loadData(context: Context,isFirst: Boolean,data: String?): Observable<HomeBean>? {
-        val retrofitClient = RetrofitClient.getInstance(context,ApiService.BASE_URL)
-        val apiService  = retrofitClient.create(ApiService::class.java)
-        when(isFirst) {
-            true -> return apiService?.getHomeData()
+class HomeModel {
+    fun loadData(context: Context, isFirst: Boolean, data: String?): Observable<HomeBean>? {
 
-            false -> return apiService?.getHomeMoreData(data.toString(), "2")
+        val retrofitClient = RetrofitClient.getInstance(context, ApiService.BASE_URL)
+        val apiService = retrofitClient.create(ApiService::class.java)
+
+        return when (isFirst) {
+            true -> apiService?.getHomeData()
+            false -> apiService?.getHomeMoreData(data.toString(), "2")
         }
     }
 }
