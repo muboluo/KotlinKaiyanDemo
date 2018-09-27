988cf075bedf691c9e403abd91afe20b2367f57a
henry <muboluo@126.com>
2018-09-27 11:02:32 +0800

    完成HotFragment

--- app/src/main/java/com/tt/lvruheng/eyepetizer/ui/fragment/HotFragment.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/ui/fragment/HotFragment.kt
@@ -1,15 +1,19 @@
 package com.tt.lvruheng.eyepetizer.ui.fragment
 
-import android.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import com.tt.lvruheng.eyepetizer.R
+import com.tt.lvruheng.eyepetizer.adapter.HotAdatpter
+import kotlinx.android.synthetic.main.hot_fragment.*
 
 
 class HotFragment : BaseFragment() {
 
-    private var fragmentList: ArrayList<Fragment>? = null
+    lateinit var fragmentList: ArrayList<Fragment>
 
     private val STRATEGY = arrayOf("weekly", "monthly", "historical")
+
+    private var tabs = listOf<String>("周", "月", "历史").toMutableList()
 
     private val TITLE = "strategy"
 
@@ -21,16 +25,22 @@ class HotFragment : BaseFragment() {
     override fun initView() {
 
         initWeekFragment()
+
+        initMonthFragment()
+
+        initHistoryFragment()
+
+        initViewPager()
     }
 
     private fun initWeekFragment() {
 
         val weekFragment = RankFragment() as Fragment
         val weekBundle = Bundle()
-        weekBundle.putString("strategy", STRATEGY[0])
+        weekBundle.putString(TITLE, STRATEGY[0])
         weekFragment.arguments = weekBundle
 
-        fragmentList?.add(weekFragment)
+        fragmentList.add(weekFragment)
     }
 
     private fun initMonthFragment() {
@@ -38,7 +48,24 @@ class HotFragment : BaseFragment() {
         val monthFragment = RankFragment() as Fragment
         val monthBundle = Bundle()
         monthBundle.putString(TITLE, STRATEGY[1])
+        monthFragment.arguments = monthBundle
 
+        fragmentList.add(monthFragment)
     }
 
+    private fun initHistoryFragment() {
+
+        val historyFragment = RankFragment() as Fragment
+        val historyBundle = Bundle()
+        historyBundle.putString(TITLE, STRATEGY[2])
+        historyFragment.arguments = historyBundle
+
+        fragmentList.add(historyFragment)
+    }
+
+    private fun initViewPager() {
+
+        vp_content.adapter = HotAdatpter(fragmentManager, fragmentList, tabs)
+        tab_layout.setupWithViewPager(vp_content)
+    }
 }
