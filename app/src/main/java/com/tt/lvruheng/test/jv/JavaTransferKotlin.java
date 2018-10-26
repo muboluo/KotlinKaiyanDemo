53357f9f4fb06bc117c158d7dc0e7efaee540b41
henry <muboluo@126.com>
2018-10-26 10:29:13 +0800

    函数接收者？？？

---
+++ app/src/main/java/com/tt/lvruheng/test/jv/JavaTransferKotlin.java
@@ -0,0 +1,40 @@
+package com.tt.lvruheng.test.jv;
+
+import android.util.Log;
+
+import com.tt.lvruheng.test.kt.KotlinTest;
+
+/**
+ * Created by henry on 2018/10/25.
+ * java 调用 kotlin 测试类
+ */
+
+public class JavaTransferKotlin {
+
+    private final String TAG = JavaTransferKotlin.class.getSimpleName();
+
+    public FiledTest test;
+
+
+    public void getKotlinField() {
+
+        KotlinTest kotlinTest = new KotlinTest();
+        String grade = kotlinTest.getGrade();
+        Log.e(TAG, "getKotlinField: " + grade);
+
+        String height = KotlinTest.Companion.getHEIGHT();
+
+        String weight = KotlinTest.WEIGHT;
+
+        kotlinTest.ktMethodTest();
+
+        KotlinTest.staticTest();//该方法会调用下面这个方法，所以是一样的
+        KotlinTest.Companion.staticTest();
+    }
+
+    public class FiledTest {
+
+        public String field1;
+    }
+
+}
---
+++ app/src/main/java/com/tt/lvruheng/test/kt/KotlinTest.kt
@@ -0,0 +1,48 @@
+package com.tt.lvruheng.test.kt
+
+import com.tt.lvruheng.test.jv.JavaTransferKotlin
+
+/**
+ * Created by henry on 2018/10/25.
+ */
+open class KotlinTest {
+
+    var name: String? = null
+    //编译器默认生成java 类型的 private 类型变量，并提供 get set 方法
+    var grade: String = "1"
+
+    //生成 public 类型变量， 不生成 get set 方法
+    @JvmField
+    protected var score: Int? = null
+
+    //生成 private 类型变量，不生成 get set
+    private var gender: Boolean = true
+
+    fun Int.foo(){}
+
+    companion object {
+
+        var test: Int? = null
+
+        test.foo
+
+        // KotlinTest.Companion.getHEIGHT()
+        val HEIGHT: String = "HEIGHT"
+
+        //KotlinTest.WEIGHT
+        @JvmField
+        val WEIGHT: String = "WEIGHT"
+
+        @JvmStatic
+        fun staticTest() {
+        }
+    }
+
+    fun ktMethodTest() {
+
+        //这里要注意一下，是否为空
+        val jtk: JavaTransferKotlin = JavaTransferKotlin()
+        val field1 = jtk.test?.field1
+
+    }
+}
